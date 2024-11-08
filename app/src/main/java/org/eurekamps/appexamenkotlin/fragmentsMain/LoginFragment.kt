e19daef29519e99822907c512eddc9357a11958b
Pablo <pablosanchezsequera@gmail.com>
2024-11-08 09:24:17 +0100

    PMDM: P1) Creacion del SplashFragment
    PMDM: P2) Configuracion de la pantalla de Splash, y posterioemente de la pantallla de Login

--- app/src/main/java/org/eurekamps/appexamenkotlin/fragmentsMain/LoginFragment.kt
+++ app/src/main/java/org/eurekamps/appexamenkotlin/fragmentsMain/LoginFragment.kt
@@ -1,23 +1,14 @@
 package org.eurekamps.appexamenkotlin.fragmentsMain
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import org.eurekamps.appexamenkotlin.R
 
+class LoginFragment : Fragment() {
 
-class SplashFragment : Fragment() {
-
-
-    lateinit var  progressBar: ProgressBar
-    lateinit var tvLoadingSplash: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,24 +20,15 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        progressBar = view.findViewById(R.id.progressBar)
-        tvLoadingSplash = view.findViewById(R.id.tvLoadingSplash)
 
 
-
-        Handler(Looper.getMainLooper()).postDelayed({
-
-            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
-
-
-        }, 3000)
     }
 
 
