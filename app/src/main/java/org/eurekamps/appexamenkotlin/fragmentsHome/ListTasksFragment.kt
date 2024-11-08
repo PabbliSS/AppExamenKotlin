27aaea0d89063e9165c34b1eb26c5b872893b88a
Pablo <pablosanchezsequera@gmail.com>
2024-11-08 11:20:19 +0100

    PMDM: P1) Creacion del SplashFragment
    PMDM: P2) Configuracion de la pantalla de Splash, y posterioemente de la pantallla de Login
    
    PMDM: P3) Creacion del RegistryFragment
    
    PMDM: P4) Configuracion de la pantalla de RegistryFragment
    
    PMDM: P5) Creacion del ProfileFragment
    
    PMDM: P6) Borrado del ProfileFragment
    
    PMDM: P7) Creacion de TaskCreatorFragment
    
    PMDM: P8) Preparacion de botones del LoginFragment
    
    PMDM: P9) Insertado de imagen dentro del SplashFragment
    
    PMDM: P10) Implementacion del google-services.json en el proyecto y susu correspondientes implementaciones
    
    PMDM: P11) Declaracion de botones de RegistryFragment, y configuracion de los mismos
    
    PMDM: P12) Creacion del navigation entre RegistryFragment y TaskCreatorFragment
    
    PMDM: P13) Creacion del LoginViewModel
    
    PMDM: P14)Creacion y configuracion del RegistryModelView
    
    PMDM: P15) Ajuste de configuracion del RegistryFragment, asi como su vinculacion con su correspondiente ViewModel
    
    PMDM: P16) Configuracion del TaskCreatorFragment
    
    PMDM: P17) Creacion del Home Activity, y traspaso de TaskCreatorFragment a HomeFragment
    
    PMDM: P18) Creacion del ListTasksFragment

---
+++ app/src/main/java/org/eurekamps/appexamenkotlin/fragmentsHome/ListTasksFragment.kt
@@ -0,0 +1,26 @@
+package org.eurekamps.appexamenkotlin.fragmentsHome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.eurekamps.appexamenkotlin.R
+
+class ListTasksFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_tasks, container, false)
+    }
+
+
+}
